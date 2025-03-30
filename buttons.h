e226b69056cdcf7e3122e7793d6b5b7f2909f59f
mehrdad-bt <mehrdad.babatabar@gmail.com>
2025-03-30 14:10:52 +0330

    Create buttons.h

---
+++ buttons.h
@@ -0,0 +1,14 @@
+#ifndef BUTTONS_H
+#define BUTTONS_H 
+
+#include <stdint.h>
+
+extern const uint16_t next_btn_normal[3200];
+extern const uint16_t next_btn_pressed[3200];
+extern const uint16_t back_btn_normal[3200];
+extern const uint16_t back_btn_pressed[3200];
+
+
+	
+#endif
+
